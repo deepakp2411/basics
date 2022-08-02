07c5677fe8252027fcfefea1fe07066eea73c25f
Deepak <bbaz2deepak@gmail.com>
2022-08-02 22:32:23 +0530

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            color: Colors.blue,
-            child: Image.network(('https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL604_SR604,400_.jpg'),alignment: Alignment.bottomCenter)
-        )
-        ),
-      ),
-    );
-    
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Container(
+                width: 400,
+                height: 400,
+                color: Colors.blue,
+                child: ClipOval(
+                  child: Image.network(
+                    'https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter',fit: BoxFit.cover,),
+                ),
+
+                // decoration: BoxDecoration(
+                //   color: Colors.blue,
+                //   borderRadius: BorderRadius.circular(860),
+                //   image: const DecorationImage(
+                //     fit: BoxFit.cover,
+                //     image: NetworkImage(
+                //         'https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter'),
+                //   ),
+                // )
+
+                // child: Image.asset(
+                // 'assets/images/man.jpg',
+                // color: Colors.red,
+                // colorBlendMode: BlendMode.colorBurn,
+                // fit: BoxFit.fitHeight,
+                // repeat: ImageRepeat.repeatX,
+                // alignment: Alignment.bottomCenter),
+                // ),
+                ),
+          ),
+        ));
   }
 }
